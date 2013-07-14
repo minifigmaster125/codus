84ed422cd8368f31638efe2e07bf151aa4b90c16
Suchaaver S. Chahal <xchaoscontrolx@gmail.com>
2013-07-13 19:31:54 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Piano::Application.config.secret_key_base = 'cc4544ea85cffcea0cd1a7b61256e46e957d1e15a799a64b4d43cb4a707317f22cc805306fbd6a655b421b37a24617c337fb07ccda0e84fa4b12993092051e5c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Piano::Application.config.session_store :cookie_store, key: '_piano_session'
